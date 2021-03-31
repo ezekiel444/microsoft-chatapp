d9cec1754c9b7e861ae1f2a0514ab8b23190f147
ezekiel444 <50213769+ezekiel444@users.noreply.github.com>
2021-03-31 02:36:45 +0200

    chat component setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
 import {ChatEngine} from 'react-chat-engine'
 import ChatFeed from './components/ChatFeed';
+import './App.css';
 
 function App() {
+
+  
   return (
   <ChatEngine 
-    height='100vh'
-    projectID='e489f4b2-e841-496e-9df0-27a37a1d1e2e'
-    userName='matomi'
-    userSecret='matomi'
+    height="100vh"
+    projectID="e489f4b2-e841-496e-9df0-27a37a1d1e2e"
+    userName="matomi"
+    userSecret="matomi"
     renderChatFeed={(chatAppProps)=><ChatFeed {...chatAppProps} />}
     />
   );
--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {sendMessage, isTyping} from 'react-chat-engine'
 
-export default function MessageForm() {
+export default function MessageForm(props) {
+    const [value, setValue] = useState('')
+    const {chatId, creds} = props
+
+    const handleChange = (event)=>{
+setValue(event.target.value)
+isTyping(props, chatId)
+    }
+
+const handleSumbit = (event)=>{
+event.preventDefault()
+const text = value.trim()
+if(text.length > 0 ) sendMessage(creds,chatId,{text})
+setValue("")
+}
+
     return (
-        <div>
-         message form   
-        </div>
+     <form className='message-form' onSubmit={handleSumbit}>
+<input type="text" className="message-input"  placeholder='send a message...' value={value} onChange={handleChange} onSubmit={handleSumbit} />
+     </form>
     )
 }
